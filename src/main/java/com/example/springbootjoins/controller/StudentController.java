52be2acc43fe2256ec5baddeff1195b42186f991
Stefan Szekely <sekestefan@gmail.com>
2023-01-17 16:48:16 +0100

    minor changes

--- src/main/java/com/example/springbootjoins/controller/StudentController.java
+++ src/main/java/com/example/springbootjoins/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootjoins.dto.StudentRequest;
@@ -14,19 +15,19 @@ import com.example.springbootjoins.entity.Student;
 import com.example.springbootjoins.service.StudentService;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
     
     @Autowired
     private StudentService studentService;
 
-    @PostMapping("/addStudent")
+    @PostMapping("")
     public Student saveStudent(@RequestBody StudentRequest studentRequest) {
         return studentService.saveStudent(studentRequest);
     }
 
-    @GetMapping("/findAllStudents")
+    @GetMapping("")
     public List<StudentResponse> findAllStudents() {
-        // use StudentResponse to return the data: 1. find all students 2. map the student to StudentResponse 3. collect the data to a list
         return studentService.findAllStudentsWithClassroom();
     }
 }
